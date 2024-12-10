e27987aec4537881188cbf0de6b40dfb38e9a1eb
nathan short <nathan@example.com>
2024-12-10 07:27:24 -0500

    day 10

--- 10/run.rb
+++ 10/run.rb
@@ -7,19 +7,12 @@ grid.each { |p,v| zeros << p if v == 0 }
 p1sum,p2sum = 0,0
 zeros.each do |z|
   hits = []
-  seen = {}
   q = [[z]]
   while q.length > 0
     path = q.pop
     point = path.last
-    seen[path] = true
     hits << path if grid[point] == 9
-    point.hvadjacent.each do |a|
-      if ! grid[a].nil? && grid[a] == grid[point]+1
-        new_path = path | [a]
-        q << new_path if ! seen.key?(new_path)
-      end
-    end
+    point.hvadjacent.each { |a| q << (path|[a]) if !grid[a].nil? && grid[a] == grid[point]+1 }
   end
   p1sum += hits.map(&:last).uniq.length
   p2sum += hits.length
