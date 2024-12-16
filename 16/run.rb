d6b56792cc234900f9aead85276fd4ecafa55666
nathan short <nathan@example.com>
2024-12-16 13:34:18 -0500

    day 16

---
+++ 16/run.rb
@@ -0,0 +1,89 @@
+require_relative '../lib/common.rb'
+require_relative '../lib/pqueue'
+
+# dijkstra to find best path
+# then bfs to find all paths - pruning if we get to a ( point,orientation ) that
+# was seen in dijkstra at a lower score
+
+def findpaths( path, point, grid, seen, allpaths, score, movingin, distances, target, best ) 
+
+  if point == target
+    allpaths << path if score == best
+    return
+  end
+
+  seen[[point.x,point.y]] = true
+
+  point.hvadjacent.each do |pp|
+    sscore = score
+    if ! seen[[pp.x,pp.y]] && grid[pp] != '#'
+
+      if ! movingin.nil?
+        dx = pp.x - point.x
+        dy = pp.y - point.y
+        thismove = dx == 0 ? :dy : :dx
+        sscore += 1
+        sscore += 1000 if thismove != movingin
+      end
+
+      key = {:p=>pp,:m=>thismove}
+      if ! distances.key?(key) || distances[key] >= sscore
+        findpaths( ( path | [pp] ), pp,grid,seen.dup,allpaths,sscore,thismove,distances,target, best ) if sscore <= best
+      end
+    end
+  end
+end
+
+
+def dodij( pq, grid, distances, visited, eend )
+
+  while ! pq.empty?
+
+    c = pq.pop
+    point = c[:p]
+    moving = c[:m]
+    visited[point] = true
+
+    return if point == eend
+
+    point.hvadjacent.each do |adj|
+
+      next if grid[adj] == '#'
+      distance = distances[c] + 1
+      dx = point.x - adj.x
+      dy = point.y - adj.y
+      thismove = dx == 0 ? :dy : :dx
+      distance += 1000 if thismove != moving
+
+      key = {p:adj,m:thismove}
+      if ! distances.key?(key) || distances[key] > distance
+        distances[key] = distance
+        pq.push(key)
+      end
+    end
+  end
+end
+
+grid = Grid.new(:io=>ARGF)
+start = grid.find { |k,v| v=='S' }.first
+eend = grid.find { |k,v| v=='E' }.first
+
+visited, distances = {},{}
+pq = PQueue.new {|x,y| distances[y] <=> distances[x] }
+
+pq.push({p:start,m: :dx})
+pq.push({p:start,m: :dy})
+visited[{p:start,m: :dx}] = true
+visited[{p:start,m: :dy}] = true
+distances[{p:start,m: :dx}] = 0
+distances[{p:start,m: :dy}] = 1000
+
+dodij( pq, grid, distances, visited, eend )
+best = distances.select{ |k,v| k[:p] == eend }.values.min
+
+allpaths = []
+score = 0
+findpaths( path=[], start, grid, seen={}, allpaths, score, :dx, distances, eend, best )
+p allpaths.flatten.uniq.count+1
+
+
